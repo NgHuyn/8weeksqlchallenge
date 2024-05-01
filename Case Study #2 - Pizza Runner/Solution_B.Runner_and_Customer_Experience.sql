89a2499ddc6566a55ef62d32bab1a6d6f55913a7
Nguyễn Hải Ngọc Huyền <94596692+NgHuyn@users.noreply.github.com>
2024-05-01 21:25:30 +0700

    Update Solution_B.Runner_and_Customer_Experience.sql

--- Case Study #2 - Pizza Runner/Solution_B.Runner_and_Customer_Experience.sql
+++ Case Study #2 - Pizza Runner/Solution_B.Runner_and_Customer_Experience.sql
@@ -18,32 +18,27 @@ ORDER BY r.runner_id;
 
 -- 3. Is there any relationship between the number of pizzas and how long the order takes to prepare?
 WITH time_prep_cte AS (
-	SELECT 
-		COUNT(c.order_id) AS number_of_pizzas,
-		c.order_id, 
-		c.order_time, 
-		r.pickup_time, 
-		TIMESTAMPDIFF(MINUTE, c.order_time, r.pickup_time) AS pickup_minutes
+	SELECT COUNT(c.order_id) AS number_of_pizzas,
+		   c.order_id, 
+           c.order_time, 
+           r.pickup_time, 
+           TIMESTAMPDIFF(MINUTE, c.order_time, r.pickup_time) AS pickup_minutes
 	FROM customer_orders_temp AS c
-	JOIN runner_orders_temp AS r
-		ON c.order_id = r.order_id
+	JOIN runner_orders_temp AS r ON c.order_id = r.order_id
 	WHERE r.distance != 0
 	GROUP BY c.order_id, c.order_time, r.pickup_time
 )
 
-SELECT 
-	number_of_pizzas,
-    AVG(pickup_minutes) AS avg_times
+SELECT number_of_pizzas,
+	   AVG(pickup_minutes) AS avg_times
 FROM time_prep_cte
 GROUP BY number_of_pizzas;
 
 -- 4. What was the average distance travelled for each customer?
-SELECT 
-	c.customer_id,
-    AVG(r.distance) AS avg_travelled_distance
+SELECT c.customer_id,
+	   AVG(r.distance) AS avg_travelled_distance
 FROM customer_orders_temp AS c
-JOIN runner_orders_temp AS r
-	ON c.order_id = r.order_id
+JOIN runner_orders_temp AS r ON c.order_id = r.order_id
 WHERE r.distance IS NOT NULL
 GROUP BY c.customer_id;
 
@@ -52,10 +47,9 @@ SELECT MAX(duration) - MIN(duration) AS the_delivery_time_difference
 FROM runner_orders_temp;
 
 -- 6. What was the average speed for each runner for each delivery and do you notice any trend for these values?
-SELECT 
-	order_id,
-	runner_id,
-	AVG(distance / (duration / 60)) AS "Avg speed (km/h)"
+SELECT order_id,
+	   runner_id, 
+       AVG(distance / (duration / 60)) AS "Avg speed (km/h)"
 FROM runner_orders_temp
 WHERE duration IS NOT NULL
 GROUP BY order_id, runner_id;
@@ -65,20 +59,17 @@ GROUP BY order_id, runner_id;
 
 -- 7. What is the successful delivery percentage for each runner?
 WITH delivery_cte AS (
-	SELECT 
-		runner_id,
-		SUM(CASE 
-				WHEN cancellation IS NULL THEN 1
-				ELSE 0
-			END) AS successful,
-		COUNT(order_id) AS total_orders
+	SELECT runner_id,
+		   SUM(CASE 
+					WHEN cancellation IS NULL THEN 1 ELSE 0
+			   END) AS successful,
+		   COUNT(order_id) AS total_orders
 	FROM runner_orders_temp
 	GROUP BY runner_id
 )
 
-SELECT 
-	runner_id,
-    (successful / total_orders)*100 AS percentage_of_successful_delivery
+SELECT runner_id,
+       (successful / total_orders)*100 AS percentage_of_successful_delivery
 FROM delivery_cte;
 
 
